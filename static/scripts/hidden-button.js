fdc474b82f2d9fbb62c884af862de34d6c9f2999
comp110-24s-003 <kris@cs.unc.edu>
2024-01-30 21:50:28 +0000

    Mirroring upstream changes.

--- static/scripts/hidden-button.js
+++ static/scripts/hidden-button.js
@@ -3,6 +3,8 @@ function show(){
         if (event.keyCode === 9) { // Check if tab key was pressed
           var button = document.getElementById("hiddenButton");
           button.style.display = "block"; // Reveal the hidden button
+          // button.focus(); // Set focus on the button
+          button.style.color = "black";
         }
       });
 
